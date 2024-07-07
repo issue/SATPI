e7cb66cac271d9574e44f6286ed970b0c6ba0844
Baris Altuncay <baris@alka-autoteile.de>
2024-07-07 21:26:24 +0200

    fix navbar

--- web/assets/js/menu.js
+++ web/assets/js/menu.js
@@ -7,15 +7,15 @@ function buildmenu() {
 	menu +=     "<span class=\"navbar-toggler-icon\"></span>";
 	menu +=   "</button>";
 	menu +=   "<div class=\"collapse navbar-collapse\" id=\"SatPINavbar\">";
-	menu +=     "<ul class=\"navbar-nav mr-auto\">";
-	menu +=       "<li class=\"nav-item\" id=\"index\"            ><a class=\"nav-link\" href=\"index.html\"><span class=\"fas fa-home\"></span> Home</a></li>";
-	menu +=       "<li class=\"nav-item\" id=\"log\"              ><a class=\"nav-link\" href=\"log.html\"><span class=\"fas fa-list-alt\"></span> Log</a></li>";
-	menu +=       "<li class=\"nav-item\" id=\"frontendoverview\" ><a class=\"nav-link\" href=\"frontendoverview.html\"><span class=\"fas fa-binoculars\"></span> Frontend Overview</a></li>";
-	menu +=       "<li class=\"nav-item\" id=\"frontend\"         ><a class=\"nav-link\" href=\"frontend.html\"><span class=\"fas fa-stream\"></span> Frontend</a></li>";
-	menu +=       "<li class=\"nav-item\" id=\"config\"           ><a class=\"nav-link\" href=\"config.html\"><span class=\"fas fa-cogs\"></span> Configure</a></li>";
-	menu +=       "<li class=\"nav-item\" id=\"wiki\"             ><a class=\"nav-link\" href=\"https://github.com/Barracuda09/SATPI/wiki\"><span class=\"fas fa-question\"></span> Wiki</a></li>";
-	menu +=       "<li class=\"nav-item\" id=\"contact\"          ><a class=\"nav-link\" href=\"contact.html\"><span class=\"fas fa-envelope-open\"></span> Contact</a></li>";
-	menu +=       "<li class=\"nav-item\" id=\"about\"            ><a class=\"nav-link\" href=\"about.html\"><span class=\"fas fa-info-circle\"></span> About</a></li>";
+	menu +=     "<ul class=\"navbar-nav col-md-auto\">";
+	menu +=       "<li class=\"nav-item\"><a id=\"index\" class=\"nav-link\" href=\"index.html\"><span class=\"fas fa-home\"></span> Home</a></li>";
+	menu +=       "<li class=\"nav-item\"><a id=\"log\" class=\"nav-link\" href=\"log.html\"><span class=\"fas fa-list-alt\"></span> Log</a></li>";
+	menu +=       "<li class=\"nav-item\"><a id=\"frontendoverview\" class=\"nav-link\" href=\"frontendoverview.html\"><span class=\"fas fa-binoculars\"></span> Frontend Overview</a></li>";
+	menu +=       "<li class=\"nav-item\"><a id=\"frontend\" class=\"nav-link\" href=\"frontend.html\"><span class=\"fas fa-stream\"></span> Frontend</a></li>";
+	menu +=       "<li class=\"nav-item\"><a id=\"config\" class=\"nav-link\" href=\"config.html\"><span class=\"fas fa-cogs\"></span> Configure</a></li>";
+	menu +=       "<li class=\"nav-item\"><a id=\"wiki\" class=\"nav-link\" href=\"https://github.com/Barracuda09/SATPI/wiki\"><span class=\"fas fa-question\"></span> Wiki</a></li>";
+	menu +=       "<li class=\"nav-item\"><a id=\"contact\" class=\"nav-link\" href=\"contact.html\"><span class=\"fas fa-envelope-open\"></span> Contact</a></li>";
+	menu +=       "<li class=\"nav-item\"><a id=\"about\" class=\"nav-link\" href=\"about.html\"><span class=\"fas fa-info-circle\"></span> About</a></li>";
 	menu +=     "</ul>";
 	menu +=     "<span class=\"navbar-text small-text-size\">";
 	menu +=       "Copyright &#169; 2014 - 2024 Marc Postema";
@@ -28,5 +28,5 @@ function buildmenu() {
 
 function setMenuItemActive(param) {
 	var obj = document.getElementById(param);
-	obj.className = "active";
+	obj.className = "nav-link active";
 }
